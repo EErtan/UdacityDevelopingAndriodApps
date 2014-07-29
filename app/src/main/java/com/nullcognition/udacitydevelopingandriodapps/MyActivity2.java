450605c60a0550a466dcd3a445907028511dfba8
ersin <ersin_the_ertan@hotmail.com>
2014-07-28 23:50:54 -0400

    Finished Lesson2

--- app/src/main/java/com/nullcognition/udacitydevelopingandriodapps/MyActivity2.java
+++ app/src/main/java/com/nullcognition/udacitydevelopingandriodapps/MyActivity2.java
@@ -14,76 +14,39 @@
 package com.nullcognition.udacitydevelopingandriodapps;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-
 
 public class MyActivity2 extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_activity2);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_my_activity2);
+		if(savedInstanceState == null){
+			getFragmentManager().beginTransaction().add(R.id.container, new ForecastFragment()).commit();
+		}
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my_activity2, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.my_activity2, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-
-	        String[] days = {"Sun","Mon","Tues","Wed","Thurs","Fri", "Sat","Sun","Mon","Tues","Wed","Thurs","Fri", "Sat","Sun","Mon","Tues","Wed","Thurs","Fri", "Sat","Sun","Mon","Tues","Wed","Thurs","Fri", "Sat"};
-	        java.util.List<String> daysList = new java.util.ArrayList<String>(java.util.Arrays.asList(days));
-
-	        android.widget.ArrayAdapter<String> arrayAdapter = new android.widget.ArrayAdapter<String>(
-			        getActivity(),
-			        com.nullcognition.udacitydevelopingandriodapps.R.layout.list_item_forcast,
-			        com.nullcognition.udacitydevelopingandriodapps.R.id.list_item_forecast_textview,
-			        daysList);
-
-            View rootView = inflater.inflate(R.layout.fragment_my_activity2, container, false);
-	        android.widget.ListView listView = (android.widget.ListView)rootView.findViewById(
-			        com.nullcognition.udacitydevelopingandriodapps.R.id.listview_forecast);
-	        listView.setAdapter(arrayAdapter);
-
-	        return rootView;
-        }
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if(id == R.id.action_settings){
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
