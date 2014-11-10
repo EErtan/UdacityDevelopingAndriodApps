ac1fe2f74817783e2054656edfef13e81d022c07
ersin <ersin_the_ertan@hotmail.com>
2014-11-09 23:55:45 -0500

    Lesson 4b - Utility class and factored out the async FetchWeatherTask to FetchWeatherTaskOut, but lots of compile errors so will need to restart app in second module.

---
+++ app/src/main/java/com/nullcognition/udacitydevelopingandriodapps/Utility.java
@@ -0,0 +1,12 @@
+package com.nullcognition.udacitydevelopingandriodapps;
+/**
+ * Created by ersin on 09/11/14 at 11:53 PM
+ */
+
+public class Utility {
+
+  public static String getPreferredLocation(android.content.Context context){
+	android.content.SharedPreferences prefs = android.preference.PreferenceManager.getDefaultSharedPreferences(context);
+	return prefs.getString(context.getString(R.string.pref_location_key), context.getString(R.string.pref_location_default));
+  }
+}
